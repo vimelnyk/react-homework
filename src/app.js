fa56a9760f7f7ec07c7941e6e2a0ad729329a6d1
Vira Melnyk <vira_melnyk@epam.com>
2019-11-29 16:58:12 +0100

    Work

--- src/app.js
+++ src/app.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './components/app';
+import App from './views';
 import './styles/style.scss';
 
 ReactDOM.render(<App />,
---
+++ src/views/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import FilmList from './film-list';
+
+export default class App extends React.Component {
+    render() {
+        return ( <
+            >
+            Show me <FilmList />
+            <
+            />
+        )
+    }
+}
